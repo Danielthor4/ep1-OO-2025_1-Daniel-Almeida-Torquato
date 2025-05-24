f7a7246c0b43a70355ea2baf1e2bfd0d06771fea
Daniel Almeida Torquato <dani.almeida.tor4@gmail.com>
2025-05-24 11:38:36 -0300

    Atualizando Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
+
+        do {
+            System.out.println("==== SISTEMA ACADÊMICO FCTE ====");
+            System.out.println("1. Gerenciar Alunos");
+            System.out.println("2. Gerenciar Disciplinas/Turmas");
+            System.out.println("3. Avaliações e Frequência");
+            System.out.println("0. Sair");
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    // chamar métodos para menu de alunos
+                    break;
+                case 2:
+                    // menu de disciplinas/turmas
+                    break;
+                case 3:
+                    // menu de avaliação/frequência
+                    break;
+                case 0:
+                    System.out.println("Encerrando...");
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+            }
+        } while (opcao != 0);
+
+        scanner.close();
     }
 }
